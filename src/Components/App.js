f41c09f7eb742a3aa7ecb243e5dd67cd83fc76fd
Troy <troyakins99@yahoo.com>
2022-01-24 18:53:14 -0600

    created about, services, and testimony files

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,18 @@
 import '../App.css';
 import Nav from './Navbar/Nav';
+import Header from './Header/Header'
+import Services from './Services/Services';
+import Testimony from './Testimony/Testimony';
+import About from './About/About';
 
 function App() {
   return (
     <div>
      <Nav/>
+     <Header/>
+     <Services/>
+     <Testimony/>
+     <About/>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
   return(
     <div>
-
+      I am the header!
     </div>
   ) 
 };
