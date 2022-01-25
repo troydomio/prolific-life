36f8e8ea6a0c282485e72b13c06a51954dcf4c6b
Troy <troyakins99@yahoo.com>
2022-01-24 19:04:04 -0600

    nav bar structure and styling

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -3,8 +3,13 @@ import './Nav.css'
 
 const Nav = () => {
   return (
-    <div>
-    I am the nav bar!
+    <div className='nav'>
+    <div className='logo'><a href="/home"><img src=""/></a></div>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/services">Services</a>
+    <a href="/contact">Contact</a>
+    <div className='book_btn'><a href="/book">Book Appointment</a></div>
     </div>
   )
 };
