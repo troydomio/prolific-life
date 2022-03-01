a1f7ccedaa19a0411959cc092160403f0cbaefbc
Troy <troyakins99@yahoo.com>
2022-02-28 19:32:21 -0600

    updated CSS for bottom sections

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,9 +7,7 @@ const About = () => {
       
         <div className='about' id="faq-about">
           <p><span><h3 className='about_text'>Meet Celisa</h3></span>
-          
           Celisa is a licensed hairstylist with 18 years of experience born and raised in Dallas, Texas. From the kitchen to Paul Mitchell's Beauty School Celisa's work revolves around establishing and maintaining healthy hair for all of her clients. Celisa specializes in natural hair and color while using professional products such as Influance (her favorite), Nairobi and Paul Mitchell. The name 'Prolific Shop' was coined by a Nipsey Hussle verse on the song Victory Lap, "I'm prolific, so gifted, I'm the type that's gone go get it". Celisa has 3 kids who she supports with her hard work, dedication, and faith over fear mentality. Her favorite bible verse is Hebrews 11:1.  </p>
-          
           <img src="https://live.staticflickr.com/65535/51904970955_8ef5b4c85b_b.jpg"/>
         </div>
     </div>
--- src/Components/Testimony/Testimony.js
+++ src/Components/Testimony/Testimony.js
@@ -9,8 +9,8 @@ const Testimony = () => {
     <div className='reviews'>
 
         <div><Clientcard img="https://live.staticflickr.com/65535/51903485067_7d68360568_b.jpg" name="Victoria" review="Push thru color! Come on Natural hair with the body! Thank you Celisa I love me hair!!!"/></div>
-        <div><Clientcard img="https://news.wayne.edu/storage/files/whitfield-spotlight-research-photo-1-607070992a723.jpg" name="Danielle Thomas" review="Very professional!"/></div>
-        <div><Clientcard img="http://hogg.utexas.edu/wp-content/uploads/2019/07/mia.png" name="Jane Doe" review="I come here every 2 weeks and can't imagine another stylist doing my hair."/></div>
+        {/* <div><Clientcard img="https://news.wayne.edu/storage/files/whitfield-spotlight-research-photo-1-607070992a723.jpg" name="Danielle Thomas" review="Very professional!"/></div>
+        <div><Clientcard img="http://hogg.utexas.edu/wp-content/uploads/2019/07/mia.png" name="Jane Doe" review="I come here every 2 weeks and can't imagine another stylist doing my hair."/></div> */}
        
     </div>
     </>
