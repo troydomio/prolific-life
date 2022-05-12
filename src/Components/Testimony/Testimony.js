d912095fad86a68df5778fd62ce483b021693156
Troy <troyakins99@yahoo.com>
2022-05-11 21:16:11 -0500

    replaced testimony images and container width for desktop view

--- src/Components/Testimony/Testimony.js
+++ src/Components/Testimony/Testimony.js
@@ -8,8 +8,9 @@ const Testimony = () => {
     <h3 className='title'>Happy Clients</h3>
     <div className='reviews'>
 
-        <div><Clientcard img="https://live.staticflickr.com/65535/51932605778_145a32d8f6_b.jpg" name="Yvette" review="You always slay my hair! Love you!"/></div>
-        <div><Clientcard img="https://live.staticflickr.com/65535/51933139805_c361a6dd40_b.jpg" name="Victoria" review="Push through color! Come on Natural hair with the body! Thank you Celisa I love my hair!!!"/></div> 
+        <div><Clientcard img="https://live.staticflickr.com/65535/52067428231_c8463be7ae_b.jpg" /></div>
+        <div><Clientcard img="https://live.staticflickr.com/65535/52067428216_786c4147ac_b.jpg" /></div> 
+        <div><Clientcard img="https://live.staticflickr.com/65535/52066384082_33ce266cfc_b.jpg"/></div> 
        
     </div>
     </>
