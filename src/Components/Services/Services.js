b62639369fb362e62cf316c7478063c7ca2d6568
Troy <troyakins99@yahoo.com>
2022-05-11 21:00:35 -0500

    swapped service images around per clients request

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -10,12 +10,13 @@ const Services = () => {
     <div className='service'>
     <p className='title'> Our Services</p>
     <div className='flexme'>
-   <Servicebox title="Natural Hair"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51931609642_e58f7fa099_b.jpg"/>
-   <Servicebox title="Hair Extensions"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51932593546_f3bb51d97b_b.jpg"/>
-   <Servicebox title="Hair Cut Services"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51934443784_00289716bf_b.jpg"/>
-   <Servicebox title="Color Services"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51933138752_271d738975_b.jpg"/>
-   <Servicebox title="Updo & Wedding"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51934120931_9b8c354781_b.jpg"/>
-   <Servicebox title="Shampoo & Style"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51933138707_5721e779f8_b.jpg"/>
+   {/* <Servicebox title="Natural Hair"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/51931609642_e58f7fa099_b.jpg"/> */}
+   <Servicebox title="Natural Hair"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52067399006_3e680d971b_b.jpg"/>
+   <Servicebox title="Hair Extensions"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52067641169_17b9b9d6c0_b.jpg"/>
+   <Servicebox title="Hair Cut Services"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52066368017_78dbe8c1b0_b.jpg"/>
+   <Servicebox title="Color Services"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52067414116_1be017f2f6_b.jpg"/>
+   <Servicebox title="Updo & Wedding"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52067644169_946f3df490_b.jpg"/>
+   <Servicebox title="Shampoo & Style"  link="https://www.schedulicity.com/scheduling/BSSED9/services" img="https://live.staticflickr.com/65535/52067412416_a00c2b5b88_b.jpg"/>
    </div>
     </div>
   ) 
